d0aadc56417c3768c00bff3839ca971b33c101bb
Cristian Malhue <cris.malhue@duocuc.cl>
2026-09-02 00:35:08 -0400

    Arreglo de header y video junto al texto

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,30 @@
 function inyectarHeader(){
+
     document.getElementById("header").innerHTML = `
-    <nav class="custom-navbar px-5 py-3">
+    <nav class="custom-navbar py-3">
         <div class="container columns is-vcentered is-mobile m-0">
             
             <!-- Sección 1: Logotipo (Izquierda) -->
-            <section class="column is-narrow p-0 df-logo-section">
-                <h1 class="logo-text">
+            <section class="column is-narrow p-0 logo-seccion">
+                <h1 class="logo-texto">
                     <img class="andlogo" src="/images/logo.webp">
                 </h1>
             </section>
             
             <!-- Sección 2: Enlaces (Centro) -->
-            <section class="column is-flex is-justify-content-center p-0 df-menu-section">
+            <section class="column is-flex is-justify-content-center p-0 menu-seccion">
                 <a class="nav-link is-active" href="#">Home</a>
+                <a class="nav-link" href="#quienes">Quienes somos</a>
                 <a class="nav-link" href="#elegirnos">Por que elegirnos</a>
                 <a class="nav-link" href="#oferta">Como trabajamos</a>
                 <a class="nav-link" href="#fotos">Galeria de eventos</a>
                 <a class="nav-link" href="#faq">FAQ</a>
                 <a class="nav-link" href="#cotizacion">Cotizacion</a>
             </section>
+
         </div>
     </nav>`;
+
 }
 
 inyectarHeader();
