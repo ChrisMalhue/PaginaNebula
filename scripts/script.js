a4bf8ad101b976ec041c30634e67dfc189ebdd45
berc-01 <cat.stols@protonmail.com>
2026-09-06 17:53:26 -0300

    Tercera sesión terminada

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Header
+
 function inyectarHeader(){
 
     document.getElementById("header").innerHTML = `
@@ -36,4 +38,62 @@ function recibirDatos(event) {
     // Deja todos los campos del formulario completamente vacíos
     event.target.reset();
 }
+
+// Footer
+
+function inyectaFooter(){
+
+  const footerHTML = `
+    <footer class="footer has-background-black has-text-white">
+    <div class="columns is-vcentered is-justify-content-space-between">
+      <div class="column is-narrow">
+        <img class="andlogo" src="/images/logo.webp">
+      </div>
+      <div class="column is-narrow ml-6">
+        <p class="title is-5">¿Necesitas ayuda?</p>
+        <br>
+        <p class="subtitle is-6">Preguntas frecuentes</p>
+        <p class="subtitle is-6">Como trabajamos</p>
+      </div>
+
+      <div class="column is-flex is-justify-content-flex-end">
+        <div class="is-flex is-flex-direction-column gap-2">
+          <span class="icon-text">
+            <span class="icon"><i class="fa-solid fa-house"></i></span>
+            <span>Av. Alguna Parte</span>
+          </span>
+          <span class="icon-text">
+            <span class="icon"><i class="fa-solid fa-phone"></i></span>
+            <span>+56911111111</span>
+          </span>
+          <span class="icon-text">
+            <span class="icon"><i class="fa-solid fa-envelope"></i></span>
+            <span>correo@gmail.com</span>
+          </span>
+        </div>
+      </div>
+      <div class="column is-flex is-justify-content-flex-end">
+        <div class="is-flex is-flex-direction-row is-align-items-center gap-3">
+          <a href="https://www.instagram.com/nevula.scl/" target="_blank">
+            <figure class="image is-48x48">
+              <img class="is-rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8HUwsS-_LkMQoYL66F7AUH5X4HwhDjzCNMg-_jlvPxQ&s=10" />
+            </figure>
+          </a>
+          <a href="https://www.youtube.com/channel/UCa-jOKgfq2FLccvHXteRdpQ" target="_blank">
+            <figure class="image is-48x48">
+              <img class="is-rounded" src="https://www.icosix.com/uploads/icons/2025/09/youtube.png" />
+            </figure>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>`;
+
+  const container = document.getElementById('footer-container');
+  if (container) {
+    container.innerHTML = footerHTML;
+  }
+}
+
+inyectaFooter();
 inyectarHeader();
